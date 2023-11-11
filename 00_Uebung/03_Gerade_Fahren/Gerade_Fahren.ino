df85069159046e85425b4faeab4ca5333769fb53
tüftelPark <leitung@tueftelpark-pilatus.ch>
2023-11-11 14:26:02 +0100

    Update Gerade_Fahren.ino
    Schreibfehler Rueckwaerts behoben

--- 00_Uebung/03_Gerade_Fahren/Gerade_Fahren.ino
+++ 00_Uebung/03_Gerade_Fahren/Gerade_Fahren.ino
@@ -2,7 +2,7 @@
 //Erweitere den Code mit pinMode(x,x); und digitalWrite(x,x); damit das Fahrzeug gerade fahren kann.
 
 const uint8_t LinkerMotorVorwaerts = 10;    // In2
-const uint8_t LinkerMotorRueckwarts = 9;  // In1
+const uint8_t LinkerMotorRueckwaerts = 9;  // In1
 const uint8_t RechterMotorVorwaerts = 6;   // In4
 const uint8_t RechterMotorRueckwaerts = 7; // In3
 
@@ -14,6 +14,6 @@ void setup() {
 
 void loop() {
   digitalWrite(LinkerMotorVorwaerts, HIGH);
-  digitalWrite(LinkerMotorRueckwarts, LOW);
+  digitalWrite(LinkerMotorRueckwaerts, LOW);
 
 }
